f68327f0c46f62771713816231fefa669cc23d23
olaf-olaf <olaf-olaf@live.nl>
2019-11-29 14:29:36 +0100

    Update resnet101.py

--- resnet101.py
+++ resnet101.py
@@ -66,7 +66,7 @@ else:
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
-model.cuda()
+# model.cuda()
 optimizer = optim.Adam(model.fc.parameters(), lr=0.003)
 
 
@@ -94,9 +94,9 @@ def train_model(epochs = 1):
 
             # print statistics AND SAVE MODEL
             running_loss += loss.item()
-            if i % 50== 49:    # print every 2000 mini-batches
+            if i % 2000== 1999:    # print every 2000 mini-batches
                 print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 50))
+                      (epoch + 1, i + 1, running_loss / 2000))
                 running_loss = 0.0
                 torch.save(model.state_dict(), "model_iteration"+str(i)+"epoch"+str(epoch)+".pl")
         print('Finished Training')
@@ -104,5 +104,5 @@ train_model()
 
 
 # SAVE LAST model
-torch.save(model.state_dict(), "first_resnet_model.p")
+torch.save(model.state_dict(), "first_resnet_model.pl")
 print("FINAL MODEL SAVED AS")
