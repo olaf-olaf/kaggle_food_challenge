19a15f05101534e75f63a65fa96159b241efdf2e
olaf-olaf <olaf-olaf@live.nl>
2019-11-30 15:51:15 +0100

    updated files

---
+++ create_kaggle_scores.py
@@ -0,0 +1,77 @@
+import torch
+from torchvision import datasets, transforms, models
+from torch import nn
+from torch.utils.data import DataLoader, Dataset
+import pandas as pd
+from PIL import Image
+
+
+
+# NOTES
+# Create train / val test_set
+# Lower learning rate when it stops decreasing. learning rate scheduler.
+# Remove relu from last layer
+class TestSetFood(Dataset):
+    def __init__(self, file_path, transform=None):
+        self.data = pd.read_csv(file_path)
+        self.data['img_name'] = "food-recognition-challenge/test_set/test_set/"+ self.data['img_name']
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        img_name = self.data['img_name'][index]
+        image = Image.open(img_name)
+        label = self.data['label'][index] - 1
+        if self.transform is not None:
+            image = self.transform(image)
+        return image, label
+
+transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+train_dataset = TestSetFood('food-recognition-challenge/sample.csv', transform)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=50,shuffle=False, num_workers=2)
+
+
+# Define whatever model you load here.
+model = models.resnet101(pretrained=True)
+model.fc = nn.Sequential(nn.Linear(2048, 512),
+                                 nn.ReLU(),
+                                 nn.Dropout(0.2),
+                                 nn.Linear(512, 80),
+                                 nn.LogSoftmax(dim=1))
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.load_state_dict(torch.load('first_resnet_model.pl', map_location=device))
+model.eval()
+
+def feed_forward():
+    prediction_labels = []
+    for i, data in enumerate(train_loader, 0):
+        print("BATCH", i)
+        # get the inputs; data is a list of [inputs, labels]
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        inputs, labels = data
+        inputs, labels = inputs.to(device),labels.to(device)
+        output = model(inputs)
+        _, predicted = output.max(dim=1)
+        # print(predicted[0].item() + 1)
+        for pred in predicted:
+            print(pred)
+            prediction_labels.append(pred.item() + 1)
+    return prediction_labels
+
+def create_submission_csv(file_path, prediction_labels, submission_name):
+    submission_csv = pd.read_csv(file_path)
+    submission_csv['label'] = prediction_labels
+    submission_csv.to_csv('submission_name')
+
+prediction_labels = feed_forward()
+create_submission_csv('food-recognition-challenge/sample.csv', prediction_labels, 'first_resnet_submision.csv')
--- resnet101.py
+++ resnet101.py
@@ -10,15 +10,13 @@ import torch.nn.functional as F
 from torchvision import datasets, transforms, models
 from PIL import Image
 
-USE_GPU = True
 NUM_CLASSES = 80
 
 class DatasetFood(Dataset):
-    def __init__(self, file_path, transform=None):
+    def __init__(self, train_set, transform=None):
 
-        self.data = pd.read_csv(file_path)
+        self.data = train_set
         self.data['img_name'] = "/kaggle/input/food-recognition-challenge/train_set/train_set/"+ self.data['img_name']
-        print("LENGTH DATASET", len(self.data['img_name']))
         self.transform = transform
 
     def __len__(self):
@@ -33,76 +31,95 @@ class DatasetFood(Dataset):
         return image, label
 
 
+def create_train_validation(file_path):
+    data = pd.read_csv(file_path)
+    train_set =data.sample(frac=0.8,random_state=200) #random state is a seed value
+    test_set = data.drop(train.index)
+    return train_set, validation_set
+
+# Prepare data
 transform = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
+train_set, validation_set = create_train_validation('/kaggle/input/food-recognition-challenge/train_labels.csv')
 
-train_dataset = DatasetFood('/kaggle/input/food-recognition-challenge//train_labels.csv', transform)
+# Put the train data in a loader
+train_dataset = DatasetFood(train_set, transform)
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64,shuffle=True, num_workers=2)
 
-
+# Put the validation data in a DataLoader
+validation_dataset = DatasetFood(validation_set, transform)
+validation_loader = torch.utils.data.DataLoader(validation_dataset, batch_size=64,shuffle=False, num_workers=2)
 
 # Load resnet101 freeze all layers, and add one extra output layer
 model = models.resnet101(pretrained=True)
 for param in model.parameters():
     param.requires_grad = False
 
+# Added a linear layer with output of 80 classes and softmax activation.
 model.fc = nn.Sequential(nn.Linear(2048, 512),
-                                 nn.ReLU(),
                                  nn.Dropout(0.2),
                                  nn.Linear(512, NUM_CLASSES),
                                  nn.LogSoftmax(dim=1))
-criterion = nn.CrossEntropyLoss()
 
-# model.cuda()
-# Try to use a gpu if available
-if USE_GPU and torch.cuda.is_available():
+
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.fc.parameters(), lr=0.001)
+
+
+# Put model on gpu if available
+if torch.cuda.is_available():
     print('using device: cuda')
 else:
     print('using device: cpu')
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
-# model.cuda()
-optimizer = optim.Adam(model.fc.parameters(), lr=0.003)
 
 
+# Train model
+epochs = 1
+for epoch in range(epochs):  # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        print("BATCH", i)
+        # get the inputs; data is a list of [inputs, labels]
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        inputs, labels = data
+        inputs,labels = inputs.to(device),labels.to(device)
 
+        # zero the parameter gradients
+        optimizer.zero_grad()
 
-# Define training
-def train_model(epochs = 1):
-    for epoch in range(epochs):  # loop over the dataset multiple times
-        running_loss = 0.0
-        for i, data in enumerate(train_loader, 0):
-            print("BATCH", i)
-            # get the inputs; data is a list of [inputs, labels]
-            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-            inputs, labels = data
-            inputs,labels = inputs.to(device),labels.to(device)
+        # forward + backward + optimize
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
 
-            # zero the parameter gradients
-            optimizer.zero_grad()
+        # print statistics AND SAVE MODEL
+        running_loss += loss.item()
 
-            # forward + backward + optimize
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+    if epoch % 100 == 99:
+            print("running loss", running_loss /len(train_set))
+            torch.save(model.state_dict(), "first_resnet_model2.0"+"epoch"+str(epoch)+".pl")
+    print('Finished Training')
 
-            # print statistics AND SAVE MODEL
-            running_loss += loss.item()
-            if i % 2000== 1999:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
-                torch.save(model.state_dict(), "model_iteration"+str(i)+"epoch"+str(epoch)+".pl")
-        print('Finished Training')
-train_model()
+torch.save(model.state_dict(), "first_resnet_model2.0.pl")
 
+# Get accuracy on validation set
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in validation_loader:
+        inputs, labels = data
+        outputs = model(inputs)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
 
-# SAVE LAST model
-torch.save(model.state_dict(), "first_resnet_model.pl")
-print("FINAL MODEL SAVED AS")
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
